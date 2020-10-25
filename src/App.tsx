72ca7312beaa7bd62c77c5b62433cdc37465a4d1
chang_su <chang_su@hunliji.com>
2020-10-25 22:17:46 +0800

    使用自己的样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>标题1</h1>
+        <h2>标题2</h2>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
