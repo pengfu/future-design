2b8ed822247283ea7062dbbe22e9417d264e3995
chang_su <chang_su@hunliji.com>
2020-10-25 23:42:16 +0800

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+// import logo from './logo.svg';
 // import './App.css';
+import Button, { ButtonType, ButtonSize } from "./components/Button/Button";
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          test
+        </Button>
+        <Button autoFocus>test</Button>
+        <Button btnType={ButtonType.Link} href="www.baidu.com">
+          test
+        </Button>
+
         <h1>标题1</h1>
         <h2>标题2</h2>
         <p>
