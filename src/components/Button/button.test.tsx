5d2ea1e1e0af2796d9359cc1a4d6f6dfc6a86c8c
chang_su <chang_su@hunliji.com>
2020-10-26 22:03:14 +0800

    setupTests.ts setup入口文件

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -5,5 +5,17 @@ import Button from "./Button";
 test("our first react test case", () => {
   const wrapper = render(<Button>Nice</Button>);
   const element = wrapper.queryByText("Nice");
-  expect(element).toBeTruthy;
+  expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+});
+
+// describe 分类 ，it等同于上面的的test
+
+describe("test Button Component", () => {
+  it("should render the correct default button", () => {
+    const wrapper = render(<Button>Nice</Button>);
+    const element = wrapper.getByText("Nice");
+    expect(element.tagName).toEqual("BUTTON");
+    expect(element).toHaveClass("btn btn-default");
+  });
 });
