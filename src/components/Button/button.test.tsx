94302faabbe6a76a38f1f909c1181f89c34c37b5
chang_su <chang_su@hunliji.com>
2020-10-26 22:08:08 +0800

    模拟函数点击

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Button from "./Button";
+
+const defaultProps = {
+  onClick: jest.fn(),
+};
 
 test("our first react test case", () => {
   const wrapper = render(<Button>Nice</Button>);
@@ -13,9 +17,12 @@ test("our first react test case", () => {
 
 describe("test Button Component", () => {
   it("should render the correct default button", () => {
-    const wrapper = render(<Button>Nice</Button>);
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>);
     const element = wrapper.getByText("Nice");
     expect(element.tagName).toEqual("BUTTON");
     expect(element).toHaveClass("btn btn-default");
+
+    fireEvent.click(element);
+    expect(defaultProps.onClick).toHaveBeenCalled();
   });
 });
